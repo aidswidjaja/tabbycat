7b3e525915e24f15920b8d51d1aabea79c104a95
aidswidjaja <36395320+aidswidjaja@users.noreply.github.com>
2024-11-10 14:29:53 +1100

    Update heroku.py

--- tabbycat/settings/heroku.py
+++ tabbycat/settings/heroku.py
@@ -38,7 +38,7 @@ if 'DJANGO_SECRET_KEY' in environ and environ.get('DISABLE_HTTPS_REDIRECTS', '')
 
 # Parse database configuration from $DATABASE_URL
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://<postgresql>'),
+    'default': dj_database_url.config(default='postgres://localhost'),
 }
 
 # ==============================================================================
